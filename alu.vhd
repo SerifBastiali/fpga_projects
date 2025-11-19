23ee773986eb496c1059d912a5bb572958e85575
SerifBastiali <ece20094@go.uop.gr>
2025-11-19 11:41:12 +0200

    Add files via upload

--- alu.vhd
+++ alu.vhd
@@ -19,11 +19,11 @@ architecture arch of alu is
     -- Internal signals
     signal ac_sel     : std_logic_vector(n-1 downto 0);
     signal db_sel     : std_logic_vector(n-1 downto 0);
-    signal add_res    : std_logic_vector(n-1 downto 0);
-    signal sub_res    : std_logic_vector(n-1 downto 0);
+    signal addsub_res : std_logic_vector(n-1 downto 0);
     signal logic_res  : std_logic_vector(n-1 downto 0);
     signal final_res  : std_logic_vector(n-1 downto 0);
-    signal cout_add, cout_sub : std_logic;
+	 signal cin        : std_logic;
+    signal cout_addsub: std_logic;
 
 begin
     --------------------------------------------------------------------
@@ -57,28 +57,19 @@ begin
     --------------------------------------------------------------------
     -- Arithmetic unit (ADD / SUB)
     --------------------------------------------------------------------
-    ADDER: adder8bit
+    ADD_SUB: adder8bit
         port map(
-            a    => ac,
-            b    => db,
-            cin  => '0',
-            s    => add_res,
-            cout => cout_add
+            a    => ac_sel,
+            b    => db_sel,
+            cin  => alus(4),
+            s    => addsub_res,
+            cout => cout_addsub
         );
-
-    SUBBER: adder8bit
-        port map(
-            a    => ac,
-            b    => not db,
-            cin  => '1',
-            s    => sub_res,
-            cout => cout_sub
-        );
-
+	  
     --------------------------------------------------------------------
     -- Logic unit (AND, OR, XOR, NOT)
     --------------------------------------------------------------------
-    LOGIC_MUX: mux4
+    MUX3_LOGIC: mux4
         generic map(n)
         port map(
             d0  => ac and db,      -- AND
@@ -90,13 +81,17 @@ begin
         );
 
     --------------------------------------------------------------------
-    -- Final multiplexer
+    -- MUX4 (2→1): final selection between Arithmetic and Logic output
+    -- ALUS(7)
     --------------------------------------------------------------------
-    with alus select
-        final_res <= 
-            add_res      when "0000001",  -- ADD
-            sub_res      when "0000011",  -- SUB
-            logic_res    when others;     -- logical ops
+    MUX_OUTPUT : mux2
+        generic map(n)
+        port map(
+            d0 => addsub_res,
+            d1 => logic_res,
+            sel => alus(7),
+            y   => final_res
+        );
 
     dout <= final_res;
 
